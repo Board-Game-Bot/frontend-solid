892beadd478359bf6e9e8dc23b24bb4b6f17f596
Yihong <liangyihong@bytedance.com>
2023-07-15 12:29:28 +0800

    feat: add 6 per a step

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -4,7 +4,7 @@ export function Counter(props: { class: string }) {
   const [count, setCount] = createSignal(0);
 
   return (
-    <button {...props} onClick={() => setCount(count() + 1)}>
+    <button {...props} onClick={() => setCount(count() + 6)}>
       count: {count()}
     </button>
   );
