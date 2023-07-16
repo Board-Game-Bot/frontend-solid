6ba9e5170e9a5c6a410edc32e1c54ef2a9c83662
Yihong <liangyihong@bytedance.com>
2023-07-16 22:26:31 +0800

    chore: use config based routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { Router, useRoutes } from '@solidjs/router';
+import { useRoutes } from '@solidjs/router';
 import routes from './config/routes';
 
 export function App() {
   const Routes = useRoutes(routes);
 
-  return (
-    <Router>
-      <Routes />
-    </Router>
-  );
+  return <Routes />;
 }
