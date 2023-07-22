87555f4e9c0407d72e2ec917ec54dcd2d0b5529e
Yihong <liangyihong@bytedance.com>
2023-07-23 01:35:59 +0800

    feat: 整理通用组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-/* @refresh reload */
 import { render } from 'solid-js/web';
 
 import './index.css';
 import 'uno.css';
+import 'virtual:uno.css';
 import App from './App';
 import { Router } from '@solidjs/router';
-import 'virtual:uno.css';
 
 const root = document.getElementById('root');
 
