d3f4e538db8c765d1c08f80e6a25246822ccb51e
SokuRitszZ <1062708758@qq.com>
2023-07-30 15:28:09 +0800

    fix: 移除 fakerjs，打包体积过大

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,33 +1,10 @@
-import { createSignal, JSX, For } from 'solid-js';
-import { faker } from '@faker-js/faker';
+import { JSX } from 'solid-js';
 import { ContentBox } from '@/components/common';
 
-function Title(props: { title: string }) {
-  return <h2 class="m-0">{props.title}</h2>;
-}
-
-function Content(props: { content: string }) {
-  return <p class="text-2xl">{props.content}</p>;
-}
-
 export default function WelcomeView(): JSX.Element {
-  const [instructions] = createSignal<[string, string][]>(
-    Array(5)
-      .fill(0)
-      .map(() => [faker.lorem.words(4), faker.lorem.paragraphs()]),
-  );
-
   return (
     <ContentBox>
       <h1>Welcome View</h1>
-      <For each={instructions()}>
-        {([title, content]) => (
-          <div class="mb-14">
-            <Title title={title} />
-            <Content content={content} />
-          </div>
-        )}
-      </For>
     </ContentBox>
   );
 }
